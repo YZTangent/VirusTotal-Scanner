50ef59e335a2e29cbecbce69f52c66304ef973c1
YZTangent <yuanzheng.tan.tyz@gmail.com>
2024-04-23 22:12:25 +0800

    feat: add logic to read file input

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -15,11 +16,42 @@ type Reports struct {
 	Report []byte
 }
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, _ := template.ParseFiles("index.html")
-		tmpl.Execute(w, map[string][]Analysis{"Analysis": {{Name: "Analysis 1"}, {Name: "Analysis 2"}}})
-	})
+var tmpl *template.Template
 
-	http.ListenAndServe(":8080", nil)
+func initPage() {
+	tmpl = template.Must(template.ParseFiles("index.html"))
+}
+
+func loadPageHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl.Execute(w, map[string][]Analysis{"Analysis": {{Name: "Analysis 1"}, {Name: "Analysis 2"}}})
+}
+
+func scanFileHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received file submission")
+
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		log.Println("Error parsing form: ", err)
+		http.Error(w, "Error parsing submission", http.StatusInternalServerError)
+	}
+
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		log.Println("Error retrieving file from form: ", err)
+		http.Error(w, "Error retrieving file from form: ", http.StatusInternalServerError)
+	}
+	defer file.Close()
+
+	log.Println("File received successfully: ", header.Filename)
+}
+
+func main() {
+
+	initPage()
+
+	http.HandleFunc("/", loadPageHandler)
+
+	http.HandleFunc("/submit", scanFileHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
